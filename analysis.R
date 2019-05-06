6dbe16ad0959a457e428c1e7439b37d88aa7cd5e
Dayoung Cheong <dcheong@uw.edu>
2019-05-06 13:56:20 -0700

    exercise

---
+++ analysis.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(leaflet)
+library(dplyr)
+library(plotly)
+
+#analyze midwest data
+
+#add midwest to a variable called data
+
+#How many rows are in data
+
+#what is the sum of the population in data
+
+#what is the sum of the asian population
+
+#Get the state with the max state_total from the data
+
+# create a scatter plot of your data 
+
+
